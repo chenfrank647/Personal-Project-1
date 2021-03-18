f8bcd0985cce188c0ea1a84cfe85e9f188e31865
chenfrank647 <chenfrank647@gmail.com>
2021-03-18 19:16:14 -0400

    Routes

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'resume', component: ResumeComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { IntroComponent } from './intro/intro.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { LoginComponent } from './login/login.component';
     IntroComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    ResumeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
